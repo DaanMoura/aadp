16e8993dfe593cecdd67cbc93028b7c14d378385
DaanMoura <daniel.lucio.moura@gmail.com>
2019-05-17 09:24:02 -0300

    resolvendo conflitos

--- aadp.pl
+++ aadp.pl
@@ -32,28 +32,28 @@ pode_passar(Pos,Pos2) :-
   not(fora_do_mapa(Pos2)).
 
 % pegando sujeira
-s([Pos, Sacola, Sujeiras], [Pos, Sacola2, Sujeiras2]) :-  	%estado (P,Sa,Su) vem antes de (P,Sa2,Su2) se
-    pertence(Pos,Sujeiras),										%Essa posicao pertence à lista de sujeiras &
-    retirar_elemento(Pos,Sujeiras,Sujeiras2),					%Sujeiras 2 é sujeira sem o 'elemento' Pos &
-    Sacola < 2,													% A sacola não está cheia 				   &
-    Sacola2 is Sacola + 1, writeln('limpou sujeira'). 			% Sa2 tem 1 lixo a mais que Sa 					
+s([Pos, Sacola, Sujeiras], [Pos, Sacola2, Sujeiras2]) :-  	  %estado (P,Sa,Su) vem antes de (P,Sa2,Su2) se
+    pertence(Pos,Sujeiras),										                %Essa posicao pertence à lista de sujeiras &
+    retirar_elemento(Pos,Sujeiras,Sujeiras2),					        %Sujeiras 2 é sujeira sem o 'elemento' Pos &
+    Sacola < 2,													                      % A sacola não está cheia 				   &
+    Sacola2 is Sacola + 1, writeln('limpou sujeira'). 			  % Sa2 tem 1 lixo a mais que Sa 					
 
 % esvaziando sacola na lixeira
-s([Pos,Sacola,Sujeiras],[Pos,Sacola2,Sujeiras]) :-			% (P,Sa,Su) vem antes de (P,Sa2,Su2) se
-  lixeira(Pos),													%há uma lixeira em P &
-  Sacola > 0,													%Tem lixo na sacola  &
-  Sacola2 is 0, writeln('esvaziou sacola').						%A nova sacola está vazia
+s([Pos,Sacola,Sujeiras],[Pos,Sacola2,Sujeiras]) :-			      % (P,Sa,Su) vem antes de (P,Sa2,Su2) se
+  lixeira(Pos),													                      %há uma lixeira em P &
+  Sacola > 0,													                        %Tem lixo na sacola  &
+  Sacola2 is 0, writeln('esvaziou sacola').						        %A nova sacola está vazia 
 
 % andando em X 
 s([p(X, Y), Sacola, Sujeiras], [p(SX, Y), Sacola, Sujeiras]) :- % (XY,Sa,Su) vem antes de (SXY,Sa,Su) se
-    (SX is X + 1 ; SX is X - 1),									% o x novo está à um passo do antigo, seja direita ou esquerda
-    pode_passar(p(X,Y),p(SX,Y)).									% X -> SX é navegável
+    (SX is X + 1 ; SX is X - 1),									              % o x novo está à um passo do antigo, seja direita ou esquerda
+    pode_passar(p(X,Y),p(SX,Y)).									              % X -> SX é navegável
 
 %subindo no elevador
 s([p(X,Y), Sacola, Sujeiras],[p(X,SY),Sacola,Sujeiras]) :-		%Andar no Y ( apenas no elevador) se
-  elevador(X),														%Há um elevador no X atual
-  (SY is Y + 1; SY is Y - 1),										%o Y novo está há um passo do antigo, subida ou descida
-  not(fora_do_mapa(p(X,SY))), writeln('subiu no elevador').			%a nova posição não está fora do mapa
+  elevador(X),														                    %Há um elevador no X atual
+  (SY is Y + 1; SY is Y - 1),										              %o Y novo está há um passo do antigo, subida ou descida
+  not(fora_do_mapa(p(X,SY))), writeln('subiu no elevador').	  %a nova posição não está fora do mapa
 
 meta([Pos, _, Lixos]) :-										%A meta é um estado onde o robo está na powerStation,
   powerstation(Pos),												%e a lista de lixos está vazia
--- listas.pl
+++ listas.pl
@@ -1,27 +1,8 @@
 % prolog
-
-<<<<<<< HEAD
-pertence(Elem,[Elem|_]).
-pertence(Elem,[_|Cauda]) :- 
-  pertence(Elem,Cauda).
-
-=======
-negativo(N) :- N < 0.
-positivo(N) :- N > 0.
-zero(N) :- 
-  not(negativo(N)), not(positivo(N)).
-
 pertence(Elem,[Elem|_]).			%pertence se é a cabeca
 pertence(Elem,[_|Cauda]) :- 		%pertence se pertence à cauda
   pertence(Elem,Cauda).						
 
-inserir(Elem,Lista,[Elem|Lista]). 		%insere no inicio
-
-inserir_ultimo(Elem,[],[Elem]).
-inserir_ultimo(Elem,[Cabeca|Cauda],[Cabeca|Cauda_Resultante]) :- 
-  inserir_ultimo(Elem,Cauda,Cauda_Resultante).
-
->>>>>>> 628b11b6dcdce5ac6f53dadb6c2f6cfdba0078cb
 retirar_elemento(Elem,[Elem|Cauda],Cauda).
 retirar_elemento(Elem,[Cabeca|Cauda],[Cabeca|Resultado]) :- 
   retirar_elemento(Elem,Cauda,Resultado).
@@ -30,51 +11,4 @@ concatena([],L,L).
 concatena([Cabeca|Cauda],L2,[Cabeca|Resultado]) :- 
   concatena(Cauda,L2,Resultado).
 
-<<<<<<< HEAD
-=======
-soma([],0).
-soma([Elem|Cauda],S) :- 				%Soma elementos da lista
-  soma(Cauda,S1), 
-  S is S1 + Elem.
 
-multiplica([],1).
-multiplica([Elem|Cauda], S) :- 
-  multiplica(Cauda,S1), 
-  S is S1 * Elem.
-
-conta([],0).
-conta([_|Cauda],N) 
-  :- conta(Cauda,N1), 
-  N is N1 + 1.
-
-conta_occorrencia(Elem,[],0).
-conta_occorrencia(Elem,[Elem|Cauda],N) :- 
-  conta_occorrencia(Elem,Cauda,N_Cauda), 
-  N is N_Cauda + 1.
-conta_occorrencia(Elem,[Cabeca|Cauda],N) :- 
-  Elem \== Cabeca, 
-  conta_occorrencia(Elem,Cauda,N).
-
-% Exercício: Dada uma lista de números, separar em duas sendo
-% uma com os positivos e o zero, e outra com os negativos
-
-separa([],[],[]).
-separa([Elem|Cauda],[Elem|Cauda_P],Cauda_N) :- 
-  separa(Cauda,Cauda_P,Cauda_N), 
-  not(negativo(Elem)).
-separa([Elem|Cauda],Cauda_P,[Elem|Cauda_N]) :- 
-  separa(Cauda,Cauda_P,Cauda_N), 
-  negativo(Elem).
-
-separa_sem_zero([],[],[]).
-separa_sem_zero([Elem|Cauda],[Elem|Cauda_P],Cauda_N) :- 
-  separa_sem_zero(Cauda,Cauda_P,Cauda_N), 
-  positivo(Elem).
-separa_sem_zero([Elem|Cauda],Cauda_P,[Elem|Cauda_N]) :- 
-  separa_sem_zero(Cauda,Cauda_P,Cauda_N), 
-  negativo(Elem).
-separa_sem_zero([Elem|Cauda],Cauda_P,Cauda_N) :- 
-  separa_sem_zero(Cauda,Cauda_P,Cauda_N), 
-  zero(Elem).
-
->>>>>>> 628b11b6dcdce5ac6f53dadb6c2f6cfdba0078cb
